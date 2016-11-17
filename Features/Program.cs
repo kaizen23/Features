c71b1181808aad0d4b9e2895e679541692f3ca1b
kaizen23 <damianis2@poczta.onet.pl>
2016-11-17 23:15:59 +0100

    Add project files.

---
+++ Features/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] developers = new Employee[]
+            {
+                new Employee {Id =1, Name = "Scott" },
+                new Employee {Id =2, Name = "Chris" }
+            };
+
+            List<Employee> sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "Ales" },
+                new Employee { Id = 4, Name = "Olek" }
+            };
+
+            foreach (var person in sales)
+            {
+                Console.WriteLine(person.Name);
+                Console.ReadKey();
+            }
+        }
+    }
+}
