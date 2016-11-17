5ad6e41c3374087553abac3d76f06b01edbb6096
kaizen23 <damianis2@poczta.onet.pl>
2016-11-17 23:39:39 +0100

    implement IEnumerable

--- Features/Program.cs
+++ Features/Program.cs
@@ -10,21 +10,22 @@ namespace Features
     {
         static void Main(string[] args)
         {
-            Employee[] developers = new Employee[]
+            IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee {Id =1, Name = "Scott" },
                 new Employee {Id =2, Name = "Chris" }
             };
 
-            List<Employee> sales = new List<Employee>()
+            IEnumerable<Employee> sales = new List<Employee>()
             {
                 new Employee { Id = 3, Name = "Ales" },
                 new Employee { Id = 4, Name = "Olek" }
             };
 
-            foreach (var person in sales)
+            IEnumerator<Employee> enumerator = sales.GetEnumerator();
+            while (enumerator.MoveNext())
             {
-                Console.WriteLine(person.Name);
+                Console.WriteLine(enumerator.Current.Name);
                 Console.ReadKey();
             }
         }
