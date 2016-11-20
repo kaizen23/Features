4b4059868f36e10157a894c151909b10baaeb2b8
kaizen23 <damianis2@poczta.onet.pl>
2016-11-20 19:53:20 +0100

    any,all and contain when return true

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -12,10 +12,18 @@ namespace Cars
         static void Main(string[] args)
         {
             var cars = ProcessFile("fuel.csv");
-            foreach(var car in cars)
-            {
-                Console.WriteLine(car.Name);
-            }
+            var query = cars.Where(c => c.Manufacture =="BMW")
+                            .Where(c => c.Year == 2016)
+                            .OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name)
+                            .Take(10);
+            var result = cars.Any(c => c.Manufacture == "Ford");
+            Console.WriteLine(result);
+
+            //foreach(var car in query)
+            //{
+            //    Console.WriteLine($"{car.Manufacture} {car.Name} : {car.Combined}");
+            //}
             Console.ReadKey();
         }
 
