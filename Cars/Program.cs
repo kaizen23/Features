7cfb0d8690cfbd7854e3302ae5659a91c8bade48
kaizen23 <damianis2@poczta.onet.pl>
2016-11-22 22:13:52 +0100

    add class generate xml

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,26 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            var records23 = ProcessFile("fuel.csv");
+            var records = ProcessFile("fuel.csv");
+            var document = new XDocument();
+            var cars = new XElement("Cars");
+
+            foreach (var  record in records)
+            {
+                var car = new XElement("Car");
+                var name = new XElement("Name",record.Name);
+                var combined = new XElement("Combined", record.Combined);
+
+                car.Add(name);
+                car.Add(combined);
+                cars.Add(car);
+
+            }
+
+            document.Add(cars);
+            document.Save("fuel.xml");
+            Console.ReadKey();
+
          
             
         }
